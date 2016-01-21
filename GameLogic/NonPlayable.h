270b57c552cc60133564ac0783307e2693eea24a
mjfalkenstein <michaeljfalkenstein@gmail.com>
2016-01-21 11:31:04 -0500

    Create NonPlayable.h

---
+++ GameLogic/NonPlayable.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "Character.h"
+#include <vector>
+#include "Item.h"
+#include "Weapon.h"
+#include "Stats.h"
+#include "Consumable.h"
+
+enum Direction {
+	N = 0,
+	NE = 1,
+	E = 2,
+	SE = 3,
+	S = 4,
+	SW = 5,
+	W = 6,
+	NW = 7
+};
+
+class NonPlayable : Character {
+
+public:
+	std::vector<Item> getInventory();
+	void addItem(Item);
+	void removeItem(Item);
+	void equip(Item);
+	Stats getStats();
+	void move(Vec2f);
+	void turn(Direction);
+	void attack(Character);
+	void use(Character, Consumable);
+	void pathTo(Vec2f);
+
+private:
+	std::vector<Item> inventory;
+	Weapon equipped;
+	Stats stats;
+};
