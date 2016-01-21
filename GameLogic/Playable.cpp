e3e3c22543a0e52e8c3d38c78f2495a2ac6ef7fa
mjfalkenstein <michaeljfalkenstein@gmail.com>
2016-01-21 11:31:30 -0500

    Create Playable.cpp

---
+++ GameLogic/Playable.cpp
@@ -0,0 +1,29 @@
+#include "Playable.h"
+
+std::vector<Item> getInventory() {
+	return inventory;
+}
+void addItem(Item) {
+
+}
+void removeItem(Item) {
+
+}
+void equip(Item) {
+
+}
+Stats getStats() {
+	return stats;
+}
+void move(Vec2f) {
+
+}
+void turn(Direction) {
+
+}
+void attack(Character) {
+
+}
+void use(Character, Consumable) {
+
+}
