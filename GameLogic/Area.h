7b3a1eae50644dfb2f0ff051c8a416ca608a7a52
Michael Falkenstein <michaeljfalkenstein@gmail.com>
2016-01-20 20:01:42 -0500

    Still just adding stuff
    How does c++ work again?

--- GameLogic/Area.h
+++ GameLogic/Area.h
@@ -1,19 +1,24 @@
 #pragma once
 
 #include "Entity.h"
+#include "Character.h"
+#include <functional>
 
-class Area {
+class Area : Entity {
 
 public:
 	float getWidth();
 	float getHeight();
 	void setWidth(float w);
 	void setHeight(float h);
+	void setCallback(Character c, std::function<void()> f);	
+	Area();
+	Area(float w, float h);
+	Area(const Area &toCopy);
 
 private:
 	float width;
 	float height;
-	Area();
-	Area(float w, float h);
-	Area(const Area &toCopy);
+	std::function<void()> event;
+	Character character;
 };
---
+++ GameLogic/Character.cpp
@@ -0,0 +1,23 @@
+#include "Character.h"
+
+Sprite* Character::getSprite() {
+	return &currentSprite;
+}
+float Character::getWidth() {
+	return width;
+}
+float Character::getHeight() {
+	return height;
+}
+int Character::getHealth() {
+	return health
+}
+void Character::setWidth(float w) {
+	width = w;
+}
+void Character::setHeight(float h) {
+	height = h;
+}
+void Character::setHealth(int h) {
+	health = h;
+}
