cdccaefc6a0edd3cc4fc3384a4962cfca5ab2de0
Kevin Jones <jkevin1@umbc.edu>
2016-01-28 14:33:05 -0500

    finished removing moved GameLogic folder

---
+++ Engine/CMakeFiles/Engine.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Engine.dir/src/Thread.cpp.o"
+  "CMakeFiles/Engine.dir/src/JobManagerTest.cpp.o"
+  "CMakeFiles/Engine.dir/src/System.cpp.o"
+  "CMakeFiles/Engine.dir/src/JobManager.cpp.o"
+  "CMakeFiles/Engine.dir/src/ThreadTest.cpp.o"
+  "../build/libEngine.pdb"
+  "../build/libEngine.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
