19a768a2c4cd33401cdbb205204c28b7ab7f7d74
Michael Falkenstein <michaeljfalkenstein@gmail.com>
2016-02-01 15:30:44 -0500

    Created Grid.h
    Also changed up Map to hold Grids now

--- Game/src/Map.cpp
+++ Game/src/Map.cpp
@@ -1,37 +1,11 @@
 #include "Map.h"
 
-Map::Map(int width, int height) {
-	w = width;
-	h = height;
-	groundTiles = new Tile*[width];
-	for (int i = 0; i < width; ++i)
-		groundTiles[i] = new Tile[height];
-
-	decorations = new Tile*[width];
-	for (int i = 0; i < width; ++i)
-		decorations[i] = new Tile[height];
-
-	airTiles = new Tile*[width];
-	for (int i = 0; i < width; ++i)
-		airTiles[i] = new Tile[height];
+Map::Map(int w, int h) : groundTiles(width, height), decorations(width, height), airTiles(width, height){
+	width = w;
+	height = h;
 }
 
-Map::Map() {
-	w = 10;
-	h = 10;
-	Map(10, 10);
-}
-
-Map::~Map() {
-	for (int i = 0; i < w; ++i)
-		delete[] groundTiles[i];
-	delete[] groundTiles;
-
-	for (int i = 0; i < w; ++i)
-		delete[] decorations[i];
-	delete[] decorations;
-
-	for (int i = 0; i < w; ++i)
-		delete[] airTiles[i];
-	delete[] airTiles;
+Map::Map() : groundTiles(0, 0), decorations(0, 0), airTiles(0, 0) {
+	width = 0;
+	height = 0;
 }
