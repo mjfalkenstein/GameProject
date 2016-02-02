91f45e129f2f1232f4597ab7529294b15ff907fd
jkevin1 <jkevin1@umbc.edu>
2016-02-01 23:49:53 -0500

    fixed improper main

--- Game/src/Game.cpp
+++ Game/src/Game.cpp
@@ -1,6 +1,6 @@
 #include "System.h"
 // #include "Engine.h" will include System, JobManager, Graphics, Etc.
 
-int main(int argc, char* argv) {
+int main(int argc, char* argv[]) {
 	println("We did it");
 }
